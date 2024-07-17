9c1211f4fd2b7fe9ce4c82dbd60565c863d9d386
muhamedragheb <eng.muhamedragheb@gmail.com>
2024-07-17 15:02:24 +0300

    Update_middlewares

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,15 +2,16 @@ module.exports = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  {
-    name: 'strapi::cors',
-    config: {
-      enabled: true,
-      headers: '*',
-      origin: ['https://dev.tecsolutiongroup.com:1338/documentation/v1.0.0'],
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    },
-  },
+  'strapi::cors',
+  // {
+  //   name: 'strapi::cors',
+  //   config: {
+  //     enabled: true,
+  //     headers: '*',
+  //     origin: ['https://dev.tecsolutiongroup.com:1338/documentation/v1.0.0'],
+  //     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  //   },
+  // },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
